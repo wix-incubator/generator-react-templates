f1b2b9d66864b1f0c5429b49c6dba24fe0d37dab
ido <idok@wix.com>
2015-01-04 13:28:04 +0200

    add npm install

--- app/index.js
+++ app/index.js
@@ -86,9 +86,9 @@ module.exports = yeoman.generators.Base.extend({
     },
 
     install: function () {
-        //this.npmInstall(['grunt', 'grunt-contrib-clean', 'grunt-contrib-watch', 'grunt-react-templates', 'grunt-eslint'], {saveDev: true});
-        //this.installDependencies({
-        //    skipInstall: this.options['skip-install']
-        //});
+        this.npmInstall(['grunt', 'grunt-contrib-clean', 'grunt-contrib-watch', 'grunt-react-templates', 'grunt-eslint'], {saveDev: true});
+        this.installDependencies({
+            skipInstall: this.options['skip-install']
+        });
     }
 });
