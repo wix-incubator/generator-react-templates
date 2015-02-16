2383c37b76f61d9bdee404b5dd0326b991872795
ido <idok@wix.com>
2015-02-16 15:55:23 +0200

    fix #1 + update dependencies

--- app/templates/commonjs/src/hello.js
+++ app/templates/commonjs/src/hello.js
@@ -2,11 +2,9 @@
 var React = require('react/addons');
 var template = require('./hello.rt.js');
 
-module.exports = function () {
-    return React.createClass({
-        displayName: 'Hello',
-        render: function () {
-            return template.apply(this);
-        }
-    });
-};
+var hello = React.createClass({
+    displayName: 'Hello',
+    render: template
+});
+
+module.exports = hello;
--- app/templates/none/src/hello.js
+++ app/templates/none/src/hello.js
@@ -2,8 +2,6 @@ var hello = (function () {
     'use strict';
     return React.createClass({
         displayName: 'Hello',
-        render: function () {
-            return helloRT.apply(this);
-        }
+        render: helloRT
     });
 }());
